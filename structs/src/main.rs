30e2f28cac803431c6a80cac7cf72e4c9b660a75
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2025-01-09 15:02:28 -0800

    finished first struct section

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,27 @@
+#![warn(dead_code)]
+#![warn(unused_variables)]
+
+struct User {
+    active: bool,
+    username: String,
+    email:String,
+    sign_in_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = build_user(String::from("randomUser123"), String::from("random@email.com"));
+
+    let user2 = User {
+        email: String::from("antoher.user.com"),
+        ..user1
+    };
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
 }
