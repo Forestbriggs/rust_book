4964f66dcaa0931175b2cd235f3e8a09e8ad6001
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2025-01-09 14:39:14 -0800

    starting structs

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    println!("the first word is: {word}");
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
