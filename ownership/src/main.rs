db20766bf23ba9fc02e8f827d5988a660065d35c
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2025-01-09 14:14:15 -0800

    starting slices

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello");
+
+    takes_ownership(&mut s);
+
+    println!("String in main: {s}");
 }
+
+fn takes_ownership(s: &mut String) {
+    println!("String in takes_ownership: {s}");
+    s.push_str(", boomers");
+}
+
+
