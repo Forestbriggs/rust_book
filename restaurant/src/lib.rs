1b1667164309905e77a7a7af585868a12c13137f
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2025-01-13 16:24:09 -0800

    finished packages and crates chapter

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,48 +1,4 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn server_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-fn deliver_order() {}
-
-mod back_of_house {
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order() {}
-}
+mod front_of_house;
 
 pub use crate::front_of_house::hosting;
 
