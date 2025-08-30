44c5470adcfd8939a6acf11b0c79e8180272da36
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2025-08-30 12:21:14 -0700

    pushed to mutable vector

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
-    let v: Vec<i32> = Vec::new();
+    let mut v: Vec<> = Vec::new();
 
 
     let v_infered = vec![1,2,3];
 
-    
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+
 }
