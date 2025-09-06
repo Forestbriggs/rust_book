48bffdffe221d67f27abd8730d19b5f146b5c496
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2025-09-06 12:46:09 -0700

    8-4 - 8-6 done

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut v = Vec::new();
 
-    let v_infered = vec![1,2,3];
+    let _v_infered = vec![1,2,3];
 
     v.push(5);
     v.push(6);
@@ -17,5 +17,12 @@ fn main() {
         None => println!("There is no third element."),
     }
 
-    
+    let _does_not_exist = &v[100];
+    let _does_not_exist = v.get(100);
+
+    let first = &v[0];
+
+    v.push(6);
+    println!("The first element is: {first}");
+
 }
